96414502b49935fd13c9cb76834581fd9da8aac9
saroyangor <dr.saroyan98@gmail.com>
2024-08-25 16:51:00 +0400

    auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { JwtStrategy } from './strategies/jwt.strategy'
+import { GoogleStrategy } from './strategies/google.strategy'
+import { YandexStrategy } from './strategies/yandex.strategy'
+
+import { UserModule } from '../user/user.module'
+import { getJwtConfig } from '../config/jwt.config'
+import { PrismaService } from '../prisma.service'
+import { UserService } from '../user/user.service'
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+	imports: [
+		UserModule,
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	],
+	controllers: [AuthController],
+	providers: [
+		AuthService,
+		PrismaService,
+		UserService,
+		JwtStrategy,
+		GoogleStrategy,
+		YandexStrategy
+	]
 })
 export class AuthModule {}
